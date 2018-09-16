c314d5b3af282ed1cb1a2e0f8cb247e38473e7fb
hpeddula <hpeddula@outlook.com>
2018-09-16 11:21:58 +0530

    Adding  BruteCollinearPoints.java

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class BruteCollinearPoints
+{
+    private int segments=0;
+    private double pqSlope,prSlope,psSlope;
+    private Point p,q,r,s;
+    private ArrayList<LineSegment> lineSegments = new ArrayList<>();
+    public BruteCollinearPoints(Point[] points)
+    {
+        int N = points.length;
+        if(points == null) throw new java.lang.IllegalArgumentException();
+        for (int i = 0; i < N ; i++)
+        {
+            for (int j = i+1; j < N; j++)
+            {
+                for (int k = j+1; k < N ; k++)
+                {
+                    for (int l = k+1; l <N ; l++)
+                    {
+                        if(points[i] == null || points[j] == null || points[k] == null || points[l] == null) throw new java.lang.IllegalArgumentException();
+                        p = points[i];
+                        q = points[j];
+                        r = points[k];
+                        s = points[l];
+                        pqSlope = p.slopeTo(q);
+                        prSlope = p.slopeTo(r);
+                        psSlope = p.slopeTo(s);
+                        if(pqSlope == prSlope && prSlope == psSlope)
+                        {
+                            segments +=1;
+                            lineSegments.add(new LineSegment(p,s));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public int numberOfSegments()
+    {
+        return segments;
+    }
+    public LineSegment[] segments()
+    {
+        LineSegment[] lineSegments1 = new LineSegment[lineSegments.size()];
+        lineSegments1 = lineSegments.toArray(lineSegments1);
+        return lineSegments1 ;
+    }
+}
