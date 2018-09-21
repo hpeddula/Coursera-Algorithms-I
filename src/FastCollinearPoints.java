17619e76624909c52716acf01ff6d8a98067c5d0
Harsha Peddula <hpeddula@osius.com>
2018-09-21 16:03:11 +0530

    Adding Changes which fetched 41/100

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -20,16 +20,17 @@ public class FastCollinearPoints
             Arrays.sort(points,p.slopeOrder());
             for (int j = 0; j < points.length; j++)
             {
-                while(j+2 <= points.length)
+                while(j+2 < points.length)
                 {
                     Double pq1Slope = p.slopeTo(points[j]);
                     Double pq2Slope = p.slopeTo(points[j+1]);
                     Double pq3Slope = p.slopeTo(points[j+2]);
-                    if(pq1Slope == pq2Slope && pq2Slope == pq3Slope )
+                    if(pq1Slope.equals(pq2Slope) && pq2Slope.equals(pq3Slope) )
                     {
                         segments +=1;
                         lineSegments.add(new LineSegment(p,points[j+2]));
                     }
+                    j +=1;
                 }
             }
         }
@@ -44,34 +45,4 @@ public class FastCollinearPoints
         lineSegments1 = lineSegments.toArray(lineSegments1);
         return lineSegments1;
     }
-    public static void main(String[] args) {
-
-        // read the n points from a file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
-        }
-
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
-
-        // print and draw the line segments
-        FastCollinearPoints collinear = new FastCollinearPoints(points);
-//        for (LineSegment segment : collinear.segments()) {
-//            StdOut.println(segment);
-//            segment.draw();
-//        }
-        StdDraw.show();
-    }
-
 }
