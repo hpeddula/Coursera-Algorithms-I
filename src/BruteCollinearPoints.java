4b0612fc8b9bf82b078c3abf840c47125b98e307
hpeddula <hpeddula@outlook.com>
2018-09-27 13:17:18 +0530

    Add the same code as office one

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -6,18 +6,19 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.ArrayList;
 
 public class BruteCollinearPoints {
-    private int segments = 0, N;
+    private int  N;
     private double pqSlope, prSlope, psSlope;
     private Point p, q, r, s;
     private ArrayList<LineSegment> lineSegments = new ArrayList<>();
+    private LineSegment[] lineSegments1;
 
     public BruteCollinearPoints(Point[] points) {
         N = points.length;
         if (points == null) throw new java.lang.IllegalArgumentException();
         for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                for (int k = j + 1; k < N; k++) {
-                    for (int l = k + 1; l < N; l++) {
+            for (int j = i + 1; j < N-1; j++) {
+                for (int k = j + 1; k < N-2; k++) {
+                    for (int l = k + 1; l < N-3; l++) {
                         if (points[i] == null || points[j] == null || points[k] == null || points[l] == null)
                             throw new java.lang.IllegalArgumentException();
                         if(points[i].compareTo(points[j]) ==0 || points[i].compareTo(points[k]) ==0 || points[i].compareTo(points[l])==0
@@ -31,8 +32,14 @@ public class BruteCollinearPoints {
                         prSlope = p.slopeTo(r);
                         psSlope = p.slopeTo(s);
                         if (pqSlope == prSlope && prSlope == psSlope) {
-                            segments += 1;
-                            lineSegments.add(new LineSegment(p, s));
+                            if(p.slopeTo(q) ==0 && p.slopeTo(r) ==0 && p.slopeTo(s) ==0)
+                            {lineSegments.add(new LineSegment(p, s));}
+                            else
+                            {
+                                lineSegments.add(new LineSegment(p,q));
+                                lineSegments.add(new LineSegment(q,r));
+                                lineSegments.add(new LineSegment(r,s));
+                            }
                         }
                     }
                 }
@@ -41,11 +48,11 @@ public class BruteCollinearPoints {
     }
 
     public int numberOfSegments() {
-        return segments;
+        return lineSegments1.length;
     }
 
     public LineSegment[] segments() {
-        LineSegment[] lineSegments1 = new LineSegment[lineSegments.size()];
+        lineSegments1 = new LineSegment[lineSegments.size()];
         lineSegments1 = lineSegments.toArray(lineSegments1);
         return lineSegments1;
     }
