cdbc567bbc7018a0ef243c8c56251d8be1212b5d
Harsha <hpeddula@outlook.com>
2018-10-02 17:07:45 +0530

    Adding Shell Sort

---
+++ src/ShellSort.java
@@ -0,0 +1,35 @@
+public class ShellSort
+{
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+        int h =1;
+        while(h < N/3) h =3*h+1;
+        while(h>=1)
+        {
+            for (int i = h; i < N; i++)
+            {
+                for (int j = i; j >=h && less(a[j],a[j-h]) ; j -=h)
+                {
+                    exch(a,j,j-h);
+                }
+            }
+            h =h/3;
+        }
+    }
+    public static boolean less(Comparable v,Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }
+    public static void exch(Comparable[] a,int i,int j)
+    {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    public static void main(String[] args)
+    {
+        String s[] = new String[]{"S","O","R","T","E","X","A","M","P","L","E"};
+        ShellSort.sort(s);
+    }
+}
