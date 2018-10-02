1286efe90598c02f39d1e026dc9b52edf6a8fe50
Harsha <hpeddula@outlook.com>
2018-10-02 15:43:25 +0530

    Understood Insertion Sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,32 @@
+public class InsertionSort
+{
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i; j >0 ; j--)
+            {
+                if(less(a[j],a[j-1]))
+                    exch(a,j,j-1);
+            }
+        }
+    }
+    public static boolean less(Comparable v,Comparable w)
+    {
+        int res = v.compareTo(w);
+        return v.compareTo(w) < 0;
+    }
+    public static void exch(Comparable[] a,int i,int j)
+    {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    public static void main(String[] args)
+    {
+        String s[] = new String[]{"S","O","R","T","E","X","A","M","P","L","E"};
+        InsertionSort.sort(s);
+    }
+}
+
