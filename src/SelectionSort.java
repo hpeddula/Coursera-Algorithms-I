b60d39c72bf3b806bcea0dcd6cb38a0df5d9bc22
Harsha <hpeddula@outlook.com>
2018-10-02 14:29:28 +0530

    Finally understood Selection Sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -6,14 +6,14 @@ public class SelectionSort
         for (int i = 0; i <N ; i++) {
             int min =i;
             for (int j = i+1; j <N ; j++)
-            {
-                if(less(a[j],a[min])) min =j;
-                exch(a,i,min);
-            }
+                if(less(a[j],a[min]))
+                    min =j;
+            exch(a,i,min);
         }
     }
     private static boolean less(Comparable v,Comparable w)
     {
+        int res = v.compareTo(w);
         return v.compareTo(w) <0;
     }
     private static void exch(Comparable[] a,int i,int j)
@@ -21,5 +21,10 @@ public class SelectionSort
         Comparable swap = a[i];
         a[i]= a[j];
         a[j]= swap;
+    }
+    public static void main(String[] args)
+    {
+        String s[] = new String[]{"S","O","R","T","E","X","A","M","P","L","E"};
+        SelectionSort.sort(s);
     }
 }
