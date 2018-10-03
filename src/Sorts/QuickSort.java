050a1f6a0bbbdc6034e1119546cc1dbab8049325
hpeddula <hpeddula@outlook.com>
2018-10-03 17:12:16 +0530

    Adding Quick Sort

---
+++ src/Sorts/QuickSort.java
@@ -0,0 +1,44 @@
+package Sorts;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class QuickSort {
+    public static int partition(Comparable[] a,int lo,int hi)
+    {
+        int i=lo,j= hi+1;
+        while (true)
+        {
+            while (less(a[++i],a[lo]))
+                if( i == hi) break;
+            while (less(a[lo],a[--j]))
+                if(j == lo) break;
+
+            if( i >=j) break;
+            exch(a,i,j);
+        }
+        exch(a,lo,j);
+        return j;
+    }
+    public static void sort(Comparable[] a)
+    {
+        StdRandom.shuffle(a);
+        sort(a,0,a.length -1);
+    }
+    private static void sort(Comparable[] a,int lo,int hi)
+    {
+        if(hi <= lo) return;
+        int j = partition(a,lo,hi);
+        sort(a,lo,j-1);
+        sort(a,j+1,hi);
+    }
+    public static boolean less(Comparable v,Comparable w)
+    {
+        return v.compareTo(w) < 0;
+    }
+    public static void exch(Comparable[] a,int i,int j)
+    {
+        Comparable swap = a[i];
+        a[i]= a[j];
+        a[j]= swap;
+    }
+}
